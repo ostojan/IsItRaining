0f3ba922d0d23806131a8a76c375bb37989fda68
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-07-31 20:10:56 +0200

    Initial Commit

---
+++ IsItRaining/IsItRainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsItRainingApp.swift
+//  IsItRaining
+//
+//  Created by ostojan on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct IsItRainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
