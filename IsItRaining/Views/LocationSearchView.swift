41ea6b0d10eefa96b5606befb7b3c7b887a53a23
Aleksander Stojanowski <root.zelatyna.0f@icloud.com>
2023-08-22 22:22:44 +0200

    LocationSearchView

---
+++ IsItRaining/Views/LocationSearchView.swift
@@ -0,0 +1,49 @@
+//
+//  LocationSearchView.swift
+//  IsItRaining
+//
+//  Created by ostojan on 22/08/2023.
+//
+
+import SwiftUI
+
+struct LocationSearchView: View {
+    @State private var locationText = ""
+    @State private var locations = [LocationData]()
+
+    var body: some View {
+        List {
+            Section {
+                TextField("City", text: $locationText)
+                    .onSubmit {
+                        Task {
+                            await searchForLocation()
+                        }
+                    }
+            }
+
+            Section {
+                if locations.isEmpty {
+                    Text("No location found")
+                } else {
+                    ForEach(locations) { location in
+                        LocationListCell(for: location)
+                    }
+                }
+            }
+        }
+    }
+
+    @MainActor
+    func searchForLocation() async {
+        do {
+            locations = try await LocationDataSource().locationData(for: locationText)
+        } catch {}
+    }
+}
+
+struct LocationSearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchView()
+    }
+}
